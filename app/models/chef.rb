73c37f4b84339e26cbaf9c8be553251ab0c2030f
David Atkins <atki1080@gmail.com>
2020-02-21 10:07:16 -0700

    Add ingredients_used method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,10 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def ingredients_used
+    dishes
+      .joins(:ingredients)
+      .distinct
+      .pluck("ingredients.name")
+  end
 end
