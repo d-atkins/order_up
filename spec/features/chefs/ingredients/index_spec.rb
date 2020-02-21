faf7ebea2e9ff4fced144a00e373e93d87ca4842
David Atkins <atki1080@gmail.com>
2020-02-21 10:23:00 -0700

    Add most popular ingredients test

--- spec/features/chefs/ingredients/index_spec.rb
+++ spec/features/chefs/ingredients/index_spec.rb
@@ -21,5 +21,27 @@ RSpec.describe "As a visitor", type: :feature do
       expect(current_path).to eq(chef_ingredients_path(chef1.id))
       expect(page).to have_content("Bun, Fat Water, Mystery meat, Rubies, Yam")
     end
+
+    it "I see the three most popular ingredients that the chef uses in their dishes" do
+      chef1 = Chef.create(name: "Chef Mike")
+      dish1 = Dish.create(name: "Rails Soup", description: "Gooood", chef: chef1)
+      dish2 = Dish.create(name: "Burger1", description: "Good AND secure", chef: chef1)
+      dish3 = Dish.create(name: "Super Burger1", description: "Good AND secure", chef: chef1)
+      dish4 = Dish.create(name: "Super Duper Burger1", description: "Good AND secure", chef: chef1)
+      ingredient1 = Ingredient.create(name: "Yam", calories: 40)
+      ingredient2 = Ingredient.create(name: "Rubies", calories: 2)
+      ingredient3 = Ingredient.create(name: "Fat Water", calories: 4000)
+      ingredient4 = Ingredient.create(name: "Bun", calories: 120)
+      ingredient5 = Ingredient.create(name: "Mystery meat", calories: 0)
+
+      dish1.ingredients << [ingredient1, ingredient2, ingredient3]
+      dish2.ingredients << [ingredient1, ingredient4, ingredient5]
+      dish3.ingredients << [ingredient1, ingredient4, ingredient5]
+      dish4.ingredients << [ingredient1, ingredient4]
+
+      visit(chef_ingredients_path(chef1.id))
+
+      within("#popular") { expect(page).to have_content("#{ingredient1.name}, #{ingredient4.name}, #{ingredient5.name}") }
+    end
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -23,5 +23,25 @@ RSpec.describe Chef, type: :model do
 
       expect(chef1.ingredients_used).to eq(["Bun", "Fat Water", "Mystery meat", "Rubies", "Yam"])
     end
+
+    it "most_popular_ingredients" do
+      chef1 = Chef.create(name: "Chef Mike")
+      dish1 = Dish.create(name: "Rails Soup", description: "Gooood", chef: chef1)
+      dish2 = Dish.create(name: "Burger1", description: "Good AND secure", chef: chef1)
+      dish3 = Dish.create(name: "Super Burger1", description: "Good AND secure", chef: chef1)
+      dish4 = Dish.create(name: "Super Duper Burger1", description: "Good AND secure", chef: chef1)
+      ingredient1 = Ingredient.create(name: "Yam", calories: 40)
+      ingredient2 = Ingredient.create(name: "Rubies", calories: 2)
+      ingredient3 = Ingredient.create(name: "Fat Water", calories: 4000)
+      ingredient4 = Ingredient.create(name: "Bun", calories: 120)
+      ingredient5 = Ingredient.create(name: "Mystery meat", calories: 0)
+
+      dish1.ingredients << [ingredient1, ingredient2, ingredient3]
+      dish2.ingredients << [ingredient1, ingredient4, ingredient5]
+      dish3.ingredients << [ingredient1, ingredient4, ingredient5]
+      dish4.ingredients << [ingredient1, ingredient4]
+
+      expect(chef1.most_popular_ingredients).to eq(["Yam", "Bun", "Mystery meat"])
+    end
   end
 end
